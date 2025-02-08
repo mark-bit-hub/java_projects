bf0f1e34ad63a29257edebeda11e07ccbb5d6446
Oyoodaniel <155971702+mark-bit-hub@users.noreply.github.com>
2025-02-08 12:03:22 +0300

    Add files via upload

---
+++ New folder/HelloWorld.java
@@ -0,0 +1,8 @@
+
+class HelloWorld {
+    // this is my first java programme
+    /* this is a multiline comment*/
+    public static void main(String[] args) {
+        System.out.println("i like pizza!"); 
+        System.out.println("its really good");}
+}
---
+++ New folder/variables.java
@@ -0,0 +1,52 @@
+public class variables{
+    public static void main(String[] args) {
+        int age=30;
+        int year=2025;
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println("The year is " + year);
+
+        double price =19.99;
+        double gpa =3.5;
+        double temprature =-12.5;
+        System.out.println("The cost of this bag is $" + price);
+        System.out.println(gpa);
+        System.out.println(temprature);
+
+
+        char grade='A';
+        char symbol='?';
+        char currency='$';
+        System.out.println(grade);
+        System.out.println(symbol);
+        System.out.println(currency);
+
+        boolean isStudent= true;
+        boolean forSale =true;
+        boolean isOnline = true;
+
+        if(isStudent){
+            System.out.println("Youre a student ");
+        }
+        else {
+            System.out.println("Youre not a student");
+        }
+
+
+        String name="oyoo daniel";
+        System.out.println(name);
+        System.out.println("Hello " + name);
+
+        String food = "omena";
+        String car ="mustang";
+        String colour = "orange";
+        System.out.println("Your favarite food is " + food);
+        System.out.println("Youre " + age + " years old.");
+        System.out.println("Your favaroite colour is " + colour + " and your car choice is " + car + " priced at " + currency + price);
+
+
+
+
+
+    }
+}
